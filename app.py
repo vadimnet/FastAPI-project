2bcbf5e061640d2e541f079208dee1ac0ffa5729
vadimnet <abakumov.978@gmail.com>
2024-12-14 09:44:14 +0300

    Все еще тест

--- app.py
+++ app.py
@@ -1,14 +1,15 @@
-from typing import Union
-from fastapi import FastAPI
-
+from fastapi import FastAPI, Body
+from fastapi.responses import FileResponse
+ 
 app = FastAPI()
-
-
+ 
 @app.get("/")
-def index():
-    return {"Hello": "World"}
-
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+def root():
+    return FileResponse("public/index.html")
+ 
+@app.post("/hello")
+#def hello(name = Body(embed=True)):
+def hello(data = Body()):
+    name = data["name"]
+    age = data["age"]
+    return {"message": f"{name}, ваш возраст - {age}"}
